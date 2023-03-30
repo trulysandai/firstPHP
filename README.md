689f04feed1a016c9d8d647614e99e2322b6e710
trulysandai <ismachatojr@hotmail.com>
2023-03-29 22:23:11 -0600

    Inicia Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # firstPHP
+DS: nom, ncasa, calle, col, mun, tel.
+
+1. Inicio
+2. Mostrar mensaje "Sus datos son: ", nom.
+3. Mostrar calle.
+4. Mostrar ncasa.
+5. Mostrar col.
+6. Mostrar mun.
+7. Mostrar tel.
+8. Fin
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Act. 17 - Servidores</title>
+    </head>
+    <body>
+        <?php
+            $nom = "Ismael Chacón Toledo";
+            $calle = "Fco. Cárdenas";
+            $ncasa = 137;
+            $col = "Lomas Precidenciales";
+            $mun = "Saltillo";
+            $tel = "844 296 1890";
+            echo "Sus datos son: $nom <br>$calle <br>$ncasa <br>$col <br>$mun <br> $tel";
+        ?>
+    </body>
+</html>
